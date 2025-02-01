669114105982754ba3ea25e14fe5b90eafbf0708
chavez <chavezmele7030@gmail.com>
2025-02-01 03:45:36 -0600

    Refactor WheelComponent layout and integrate confetti effect for winner announcement

--- src/components/wheel.jsx
+++ src/components/wheel.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Wheel } from 'react-custom-roulette';
 import * as XLSX from 'xlsx';
-import logo from '../../src/logo.svg';
+import  Confetii  from 'react-confetti';
 
 const WheelComponent = () => {
     const [members, setMembers] = useState([]);
@@ -76,94 +76,110 @@ const WheelComponent = () => {
         setRouletteData(addShortString);
     }, [members]);
 
-    return (
-        <div className="flex h-screen w-screen relative">
-            <div className="flex flex-col items-center w-1/2 p-4">
-                <div className="flex items-center space-x-10">
-                    <img src={logo} alt="spin" className='w-[150px] h-[150px] spin' />
-                    <h2 className="text-5xl font-semibold text-white">
-                        Rifa Emprendedores del Desierto BNI
-                    </h2>
-                </div>
-                {rouletteData.length > 0 && (
-                    <div className='w-full mt-10 pl-[135px]'>
-                        <h3 className="text-xl font-semibold mb-2 text-white">Miembros ({members.length}):</h3>
-                        <div className="overflow-y-auto h-[380px] p-4 rounded-md shadow-sm">
-                            <ul>
-                                {members.map((member, index) => (
-                                    <li key={index} className="text-white">-{member}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                )}
-                <div className='flex items-center justify-center w-full'>
-                    <label className="mt-4">
-                        <span className="sr-only">Choose file</span>
-                        <input
-                            type="file"
-                            accept=".xlsx, .xls"
-                            onChange={handleFileUpload}
-                            className="block w-full text-sm text-gray-500
-                                    file:mr-4 file:py-2 file:px-4
-                                    file:rounded-full file:border-0
-                                    file:text-sm file:font-semibold
-                                    file:bg-blue-50 file:text-black
-                                    hover:file:bg-red-50"
-                        />
-                    </label>
-                </div>
-            </div>
+    
 
-            <div className="flex justify-center items-center w-1/2 h-full">
-                {rouletteData.length > 0 && (
-                    <div className="flex flex-col items-center justify-center w-full h-full">
-                        <div align="center" className="roulette-container">
-                            <Wheel
-                                mustStartSpinning={mustSpin}
-                                prizeNumber={prizeNumber}
-                                data={rouletteData}
-                                outerBorderColor={["#ccc"]}
-                                outerBorderWidth={[3]}
-                                innerBorderColor={["#f2f2f2"]}
-                                radiusLineColor={["transparent"]}
-                                radiusLineWidth={[1]}
-                                textColors={["#f5f5f5"]}
-                                textDistance={55}
-                                fontSize={[10]}
-                                backgroundColors={[
-                                    "#141414", "#030303",
-                                ]}
-                                onStopSpinning={() => {
-                                    setMustSpin(false);
-                                    setWinner(rouletteData[prizeNumber].completeOption);
-                                }}
-                            />
-                            <button
-                                className="button roulette-button"
-                                onClick={handleSpinClick}
-                                disabled={mustSpin}
-                            >
-                                <img src={logo} alt="spin" />
-                            </button>
-                            {!mustSpin && winner && (
-                                <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75">
-                                    <div className="bg-white p-8 rounded-lg shadow-lg text-center">
-                                        <p className="text-2xl font-semibold text-black">
-                                            Ganador: {winner}
-                                        </p>
-                                        <button
-                                            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md"
-                                            onClick={() => setWinner(null)}
-                                        >
-                                            Cerrar
-                                        </button>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
+    return (
+        <div className="flex flex-col h-screen w-screen">
+            <header className="flex items-center justify-between w-full bg-[#141414] p-4 px-28 shadow-xs shadow-gray-200 sticky top-0 z-50">
+                <h2 className="text-xl font-semibold text-white text-center">
+                    BNI - Emprendedores del Desierto
+                </h2>
+                <div className='flex items-center justify-center border border-white p-2 rounded-md'>
+                    <label htmlFor="archivo" className="cursor-pointer text-white rounded-md flex items-center">
+                        <img src={`${process.env.PUBLIC_URL}/icons/description.svg`} alt='file' className='mr-2' />
+                        Subir Excel
+                    </label>
+                    <input
+                        id="archivo"
+                        type="file"
+                        accept=".xlsx, .xls"
+                        onChange={handleFileUpload}
+                        className="hidden"
+                    />
+                </div>
+            </header>
+            <div className="flex flex-grow">
+                {/* Contenedor izquierdo */}
+                <div className="flex items-center justify-center w-1/2 min-h-screen">
+                    <div className="flex flex-col items-start mx-16 mb-40">
+                        <img src={`${process.env.PUBLIC_URL}/icons/softii_white.svg`} alt="roulette" />
+                        <span className="text-white text-4xl mt-[22px]">¿Quién será el</span>
+                        <span className="text-white text-4xl font-semibold">Ganador 💫✨</span>
+                        <span className="text-white text-3xl">de ésta semana?</span>
+                        <button
+                            onClick={handleSpinClick}
+                            disabled={mustSpin}
+                            className="rounded-md p-2 bg-[#ED6A5A] cursor-pointer text-white font-semibold mt-[22px]"
+                        >
+                            Elegir Ganador
+                        </button>
                     </div>
-                )}
+                </div>
+
+                {/* Contenedor derecho */}
+                <div className="flex justify-center items-center w-1/2">
+                    {rouletteData.length > 0 && (
+                        <div className="flex flex-col items-center justify-center w-full h-full">
+                            <div align="center" className="roulette-container relative">
+                                <Wheel
+                                    mustStartSpinning={mustSpin}
+                                    prizeNumber={prizeNumber}
+                                    data={rouletteData}
+                                    outerBorderColor={["#ccc"]}
+                                    outerBorderWidth={[3]}
+                                    innerBorderColor={["#f2f2f2"]}
+                                    radiusLineColor={["transparent"]}
+                                    radiusLineWidth={[1]}
+                                    textColors={["#f5f5f5"]}
+                                    textDistance={65}
+                                    fontSize={[10]}
+                                    backgroundColors={["#141414", "#030303",]}
+                                    startingOptionIndex={prizeNumber}
+                                    onStopSpinning={() => {
+                                        setMustSpin(false);
+                                        setWinner(rouletteData[prizeNumber].completeOption);
+
+                                    }}
+                                    pointerProps={{
+                                        src: `${process.env.PUBLIC_URL}/icons/pointerSpinner.svg`,
+                                        style: {
+                                            position: 'absolute',
+                                            bottom: '0',
+                                            right: '0',
+                                            transform: 'rotate(157deg)',
+                                      },
+                                    }}
+                                />
+                                <button
+                                    className="button roulette-button"
+                                    onClick={handleSpinClick}
+                                    disabled={mustSpin}
+                                >
+                                    <img src={`${process.env.PUBLIC_URL}/icons/softii_white_logo.svg`} alt="spin" />
+                                </button>
+                                {!mustSpin && winner && (
+                                    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black" style={{ top: '64px', left: 0, right: 0, bottom: 0 }}>
+                                        {winner && <Confetii
+                                            size={4}
+                                            shape="circle"
+                                            colors={['#f44336', '#9c27b0', '#3f51b5']}
+                                            wind={0.01}
+                                            gravity={0.2}
+                                        /> }
+                                        <div className="bg-black p-8 shadow-lg text-center w-[100vh - 58px]">
+                                            <span className="text-white text-[72px]">¡Ganaste!</span>
+                                            <p className="font-semibold text-white text-[110px]">{winner}</p>
+                                            <div className="flex justify-center">
+                                                <button className="rounded-md p-2 bg-[#141414] cursor-pointer text-white font-semibold mt-[22px] mr-6" onClick={() => setWinner(null)}>Regresar</button>
+                                                <button onClick={() => { handleSpinClick(); setWinner(null); }} className="rounded-md p-2 bg-[#ED6A5A] cursor-pointer text-white font-semibold mt-[22px]" > Volver a Girar </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
